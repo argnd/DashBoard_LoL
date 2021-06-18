84bdc3ca39a9d612f53f8fb5c662b43c1f43fe05
argnd <gandour37@gmail.com>
2021-06-18 13:39:38 +0200

    run

--- src/main/java/com/hello/spring/HelloSpringApplication.java
+++ src/main/java/com/hello/spring/HelloSpringApplication.java
@@ -1,13 +1,22 @@
 package com.hello.spring;
 
+import com.hello.spring.model.Hero;
+import com.hello.spring.model.Team;
+import com.hello.spring.model.User;
+import com.hello.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class HelloSpringApplication implements CommandLineRunner {
 
+    @Autowired
+    private  UserRepository userRepository;
 
 
     public static void main(String[] args) {
@@ -18,7 +27,29 @@ public class HelloSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        User tmp = new User();
+        User tmp2 = new User();
+        Team tmpt = new Team();
+        Team tmpt2 = new Team();
+        Hero tmph = new Hero();
+        List<Hero> tmpset = new ArrayList<>();
+        List<Hero> tmpset2 = new ArrayList<>();
+        tmpset.add(tmph);
+        tmpset.add(tmph);
+        tmpset.add(tmph);
+        tmpset.add(tmph);
+        tmpset2.add(tmph);
+        tmpset2.add(tmph);
+        tmpset2.add(tmph);
+        tmpset2.add(tmph);
+        tmp.setUsername("Hello");
+        tmpt.setHeroes(tmpset);
+        tmpt2.setHeroes(tmpset2);
+        tmp.setTeam(tmpt);
+        tmp2.setTeam(tmpt2);
+        userRepository.save(tmp);
+        userRepository.save(tmp2);
+        userRepository.delete(tmp);
     }
 
 
--- src/main/java/com/hello/spring/controller/HelloController.java
+++ src/main/java/com/hello/spring/controller/HelloController.java
@@ -1,26 +1,14 @@
 package com.hello.spring.controller;
 
-import com.hello.spring.model.Hero;
-import com.hello.spring.model.Team;
-import com.hello.spring.model.User;
-import com.hello.spring.repository.UserRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-
 @Controller
 public class HelloController {
 
-    private final UserRepository userRepository;
 
-    public HelloController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @GetMapping("/")
     public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -37,34 +25,6 @@ public class HelloController {
     @PostMapping(value = "/hello")
     public String greetHim(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        User tmp = new User();
-        User tmp2 = new User();
-        Team tmpt = new Team();
-        Team tmpt2 = new Team();
-        Hero tmph = new Hero();
-        List<Hero> tmpset = new ArrayList<>();
-        List<Hero> tmpset2 = new ArrayList<>();
-        tmpset.add(tmph);
-        tmpset.add(tmph);
-        tmpset.add(tmph);
-        tmpset.add(tmph);
-        tmpset2.add(tmph);
-        tmpset2.add(tmph);
-        tmpset2.add(tmph);
-        tmpset2.add(tmph);
-        tmp.setUsername("Hello");
-        tmpt.setHeroes(tmpset);
-        tmpt2.setHeroes(tmpset2);
-        tmp.setTeam(tmpt);
-        tmp2.setTeam(tmpt2);
-        userRepository.save(tmp);
-        userRepository.save(tmp2);
-        userRepository.delete(tmp);
         return "hello";
     }
 
