794743a3bb662cdb7cc46327d5b7a2c2563110c2
argnd <gandour37@gmail.com>
2021-06-23 09:45:46 +0200

    local

--- src/main/java/com/hello/spring/HelloSpringApplication.java
+++ src/main/java/com/hello/spring/HelloSpringApplication.java
@@ -1,6 +1,8 @@
 package com.hello.spring;
 
 
+import com.hello.spring.model.Hero;
+import com.hello.spring.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.CommandLineRunner;
