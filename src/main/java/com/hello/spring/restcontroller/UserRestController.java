29a7255f8bbfeec5334c0102f7b7da35eed9547d
argnd <gandour37@gmail.com>
2021-07-06 15:48:22 +0200

    Update UserRestController.java
    cleaning

--- src/main/java/com/hello/spring/restcontroller/UserRestController.java
+++ src/main/java/com/hello/spring/restcontroller/UserRestController.java
@@ -1,6 +1,5 @@
 package com.hello.spring.restcontroller;
 
-import com.hello.spring.model.User;
 import com.hello.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,14 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserRestController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserRestController(UserRepository userRepository) {
@@ -24,7 +22,7 @@ public class UserRestController {
 
     @GetMapping("/api/users")
     public Map<String,Object> home() {
-        Map<String, Object> model = new HashMap<String, Object>();
+        Map<String, Object> model = new HashMap<>();
         model.put("id", userRepository.getById(1).getId());
         return model;
     }
