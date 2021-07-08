677b6b722f47a64aac7ff2ca8282ae6a90a4c397
argnd <gandour37@gmail.com>
2021-07-08 12:50:00 +0200

    Manual Merge Local_RLI

--- src/main/java/com/hello/spring/controller/ProfilController.java
+++ src/main/java/com/hello/spring/controller/ProfilController.java
@@ -1,24 +1,77 @@
 package com.hello.spring.controller;
 
+import com.hello.spring.model.User;
+import com.hello.spring.repository.UserRepository;
 import com.hello.spring.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ProfilController {
 
     SecurityService securityService;
+    UserRepository userRepository;
 
     @Autowired
-    public ProfilController(SecurityService securityService) {
+    public ProfilController(SecurityService securityService,UserRepository userRepository) {
         this.securityService = securityService;
+        this.userRepository = userRepository;
     }
+
+    @Autowired
+
 
     @GetMapping("/profile")
     public String getProfil() {
         return "layouts/profile/profile";
+    }
+
+    @GetMapping("/avatar-modify")
+    public String getModifiedAvatar(){
+        return "layouts/profile/avatar";
+    }
+
+    @GetMapping("/mail-modify")
+    public String getModifiedMail(){
+        return "layouts/profile/mail";
+    }
+    @GetMapping("/wallp-modify")
+    public String getModifiedWallpaper(){
+        return "layouts/profile/wallpaper";
+    }
+
+    @PostMapping("/mail-modify")
+    public String changeMail (@RequestParam("emailForm")String email, @RequestParam("mdpForm")String password, HttpSession session){
+        User user = (User)session.getAttribute("user");
+        user.setEmail(email);
+        user.setPassword(password);
+        userRepository.save(user);
+        return "redirect:/profile";
+
+    }
+
+
+    @PostMapping("/avatar-modify")
+    public String changeAvatar(@RequestParam("avatarForm")String avatar, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        user.setAvatar(avatar);
+        userRepository.save(user);
+        return "redirect:/profile";
+    }
+
+    //
+    @PostMapping("/wallp-modify")
+    public String changeWallp(@RequestParam("wallpForm")String wallp, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        user.setEmail(wallp);
+        userRepository.save(user);
+        return "redirect:/profile";
     }
 
     @GetMapping("/jwt")
