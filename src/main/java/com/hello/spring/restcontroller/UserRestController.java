8cb251634a1d1975358b847b60852bc8e48725f1
argnd <gandour37@gmail.com>
2021-07-13 17:30:09 +0200

    Ports

--- src/main/java/com/hello/spring/restcontroller/UserRestController.java
+++ src/main/java/com/hello/spring/restcontroller/UserRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4402")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserRestController {
 
     private final UserRepository userRepository;
