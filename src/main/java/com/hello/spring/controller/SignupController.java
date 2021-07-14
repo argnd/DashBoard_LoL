c1059c11859795b2f0f5bee14649a168315e0135
RLI <36658379+Daishi37@users.noreply.github.com>
2021-07-14 19:47:15 +0200

    signup

--- src/main/java/com/hello/spring/controller/SignupController.java
+++ src/main/java/com/hello/spring/controller/SignupController.java
@@ -1,8 +1,57 @@
 package com.hello.spring.controller;
 
-
+import com.hello.spring.model.User;
+import com.hello.spring.repository.UserRepository;
+import com.hello.spring.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 @Controller
 public class SignupController {
+
+
+
+    @Autowired
+    SecurityService securityService;
+    @Autowired
+    UserRepository userRepository;
+
+
+
+    @GetMapping("/signup")
+    public String getSignup() {
+        return "layouts/signup/signup";
+    }
+
+    @GetMapping("/signed")
+    public String getSigned() {
+        return "layouts/signup/login";
+    }
+
+    @PostMapping("/signed")
+    public String getRegistered(@RequestParam("username") String name,@RequestParam("password") String password,@RequestParam("mail") String mail){
+        User user = new User();
+        user.setEmail(mail);
+        user.setUsername(name);
+        user.setPassword(password);
+        user.setActive(true);
+        user.setRoles("ROLE_USER");
+
+    userRepository.save(user);
+
+
+    return "redirect:/login";
+
+
+
+    }
 }
